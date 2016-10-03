0706189420df0da37766f225374056fdc26c4682
Andrew Puig <andrew.lpuig@gmail.com>
2016-10-03 11:27:53 -0400

    Tweaks
    Some minor tweaks. I wont be updating this anymore.

--- src/matchmediaload.js
+++ src/matchmediaload.js
@@ -9,7 +9,6 @@ var MatchMediaLoad = (function(window){
 
     'use strict';
 
-
     var settings = {
 
         /**
@@ -70,40 +69,7 @@ var MatchMediaLoad = (function(window){
 
     };
 
-
-    /*
-    Thanks to David Walsh for the debounce()
-    [https://davidwalsh.name/javascript-debounce-function]
-    */
-    var debounce = function(func, wait, immediate) {
-
-        var timeout;
-
-
-        return function() {
-
-            var context = this;
-
-            var args = arguments;
-
-            var later = function() {
-                timeout = null;
-                if (!immediate){
-                    func.apply(context, args);
-                }
-            };
-
-            var callNow = immediate && !timeout;
-
-            clearTimeout(timeout);
-
-            timeout = setTimeout(later, wait);
-
-            if (callNow){
-                func.apply(context, args);
-            }
-        };
-    };
+    var timeout;
 
 
     var _bindUI = function(){
@@ -111,15 +77,18 @@ var MatchMediaLoad = (function(window){
         // The browser supports matchMedia so proceed.
         if(settings._hasSupport){
 
-            var debounceResize = debounce(function(){
-                _checkAndReplace();
-            }, settings.debounceRate);
-
-
+            // Initial check.
             _checkAndReplace();
 
+            // Check on resize of course.
             window.onresize = function(){
-                debounceResize();
+
+                clearTimeout(timeout);
+
+                timeout = setTimeout(function(){
+                    _checkAndReplace();
+                }, settings.debounceRate);
+
             };
 
         } else {
@@ -133,17 +102,17 @@ var MatchMediaLoad = (function(window){
 
     var _checkAndReplace = function(){
 
+        // This wil store the elements that have matching media queries.
         var accepted = [];
-
 
         for (var i = 0, l = settings.selector.length; i < l; i++) {
 
             // Make sure we dont check the same media object
             // more than once.
             if(settings._cache.indexOf(settings.selector[i]) === -1){
-                
+
                 // Check if the media query matches.
-                if(window.matchMedia(settings.selector[i].getAttribute(settings.mediaQuery)).matches){
+                if(matchMedia(settings.selector[i].getAttribute(settings.mediaQuery)).matches){
 
                     // Add the media object to the accepted array.
                     accepted.push(settings.selector[i]);
@@ -176,7 +145,7 @@ var MatchMediaLoad = (function(window){
                 // Loop through video sources.
                 for (var ii = 0, ll = items[i].getAttribute(settings.src).split(", ").length; ii < ll; ii++) {
 
-                    // Check for a valid video file extension and 
+                    // Check for a valid video file extension and
                     // check for the first available video format that can be played in the browser.
                     if(items[i].getAttribute(settings.src).split(", ")[ii].search(settings._video.FORMAT) !== -1 && items[i].canPlayType("video/" + items[i].getAttribute(settings.src).split(", ")[ii].match(settings._video.FORMAT)[1]) !== ""){
 
@@ -207,7 +176,6 @@ var MatchMediaLoad = (function(window){
             }
 
         }
-
 
         // settings.after() returns the replaced element.
         if(typeof(settings.after) === "function"){
@@ -243,8 +211,7 @@ var MatchMediaLoad = (function(window){
             }
 
             settings.after = options.after || undefined;
-            
-            
+
             // Use the user augmented settings
             _bindUI(settings);
 
